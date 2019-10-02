a2a6c64a23f536de427507ed30d0c2d8b04b085e
Stephen R Pietrowicz <srp@illinois.edu>
2019-10-02 13:47:31 -0500

    Fix to allow command line argument for each command Add simple full sequence script

--- bin.src/endReadout.sh
+++ bin.src/endReadout.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python atevent.py endReadout --imageSequenceName SequenceName --imagesInSequence 1 --imageName AT_O_20190917_6 --imageSource AT --imageController "OOC" --imageDate "082419" --imageNumber 1 --timeStampAcquisitionStart 1.0 --exposureTime 1.0 --imageIndex 1
+python atevent.py endReadout --imageSequenceName SequenceName --imagesInSequence 1 --imageName $1 --imageSource AT --imageController "OOC" --imageDate "082419" --imageNumber 1 --timeStampAcquisitionStart 1.0 --exposureTime 1.0 --imageIndex 1
 
