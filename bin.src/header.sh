6b82a5e38ac54c14afa69f6e3c2653d5956e48f0
Stephen R Pietrowicz <srp@illinois.edu>
2019-10-10 15:48:44 -0500

    Fix header to take command line argument and use it for the header

--- bin.src/header.sh
+++ bin.src/header.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #python3 atevent.py largeFileObjectAvailable -b 1 -c 1 -g "AtHeaderService" -m 1 -u "http://141.142.238.74:8000/AT_O_20190312_000007.header" -v 1.0 -i AT_O_20190312_000007
-python3 atevent.py largeFileObjectAvailable -b 1 -c 1 -g "AtHeaderService" -m 1 -u "http://localhost:8000/ats.header" -v 1.0 -i $1
+python3 atevent.py largeFileObjectAvailable -b 1 -c 1 -g "AtHeaderService" -m 1 -u "http://141.142.238.174:8000/$1.header" -v 1.0 -i $1
