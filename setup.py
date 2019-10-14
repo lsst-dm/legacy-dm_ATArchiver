1c1897875c9b725c79045b75bffc39d5cc582ca3
Stephen R Pietrowicz <srp@illinois.edu>
2019-10-14 17:10:54 -0500

    Fix typo in command name

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     install_requires=install_requires,
     package_dir={"": "python"},
     packages=find_namespace_packages(where="python"),
-    scripts=["bin/run_at_archiver_csc.py", "bin/run_archiver_controller.py", "bin/atevent.py"],
+    scripts=["bin/run_at_archiver_csc.py", "bin/run_archive_controller.py", "bin/atevent.py"],
     license="GPL",
     project_urls={
         "Bug Tracker": "https://jira.lsstcorp.org/secure/Dashboard.jspa",
