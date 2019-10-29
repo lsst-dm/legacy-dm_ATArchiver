7e660cddae83fe08ed2fd34bd4bd0f4f40845727
Stephen R Pietrowicz <srp@illinois.edu>
2019-10-28 22:32:35 -0500

    Add run scripts to setup environment for running as service

---
+++ bin/run_atarchiver.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source $DM_ATARCHIVER_DIR/bin/setup_atarchiver.sh
+export IIP_LOG_DIR=/var/log/iip
+LOGPATH=/tmp/ospl_logs.$$
+mkdir $LOGPATH
+export OSPL_LOGPATH=$LOGPATH
+run_at_archiver_csc.py
