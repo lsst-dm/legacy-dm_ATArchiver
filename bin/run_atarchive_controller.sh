912a6943f7d4586cba7221aa5424f7c9138eaffb
Stephen R Pietrowicz <srp@illinois.edu>
2019-10-28 22:55:19 -0500

    Fix initialization Rename run script for atarchive controller

--- bin/run_atarchive_controller.sh
+++ bin/run_atarchive_controller.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#source $DM_ATARCHIVER_DIR/bin/setup_atarchiver.sh
+source $DM_ATARCHIVER_DIR/bin/setup_atarchiver.sh
 export IIP_LOG_DIR=/var/log/iip
 LOGPATH=/tmp/ospl_logs.$$
 mkdir $LOGPATH
 export OSPL_LOGPATH=$LOGPATH
-run_archiver_controller.py
+run_atarchive_controller.py
