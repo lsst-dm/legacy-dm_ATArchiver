e54004e18450f89738c0c3a6c64eda06f652a2c5
Stephen R Pietrowicz <srp@illinois.edu>
2020-10-22 15:24:15 -0500

    Changed for the switch to ts_salobj 6

--- bin/run_at_archiver_csc.py
+++ bin/run_at_archiver_csc.py
@@ -3,5 +3,4 @@ import asyncio
 from lsst.dm.ATArchiver.atarchiver_csc import ATArchiverCSC
 
 
-csc = ATArchiverCSC(index=None)
-asyncio.get_event_loop().run_until_complete(csc.done_task)
+asyncio.run(ATArchiverCSC.amain(index=None))
